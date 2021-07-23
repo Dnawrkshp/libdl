8d3383bd9c703946dab7776d0858cd185f6171e5
Metroynome <yort124@gmail.com>
2021-07-23 00:26:47 -0400

    memcard functions added

--- include/mc.h
+++ include/mc.h
@@ -17,6 +17,11 @@ int McClose(int fd);
 int McSeek(int fd, int offset, int origin);
 int McRead(int fd, void *buffer, int size);
 int McWrite(int fd, const void *buffer, int size);
+int McMkDir(int port, int slot, const char *name);
+int McDelete(int port, int slot, const char *name);
+int McFormat(int port, int slot);
+int McUnformat(int port, int slot);
+
 int McPollSema(void);
 __LIBDL_GETTER__ int McGetResult(void);
 
