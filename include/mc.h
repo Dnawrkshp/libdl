784ee60609fb6372f0d1821431707c2d2df009a0
Metroynome <yort124@gmail.com>
2021-07-23 01:00:04 -0400

    more memcard updates

--- include/mc.h
+++ include/mc.h
@@ -17,12 +17,12 @@ int McClose(int fd);
 int McSeek(int fd, int offset, int origin);
 int McRead(int fd, void *buffer, int size);
 int McWrite(int fd, const void *buffer, int size);
+int McSync(int mode, int *cmd, int *result);
 int McGetInfo(int port, int slot, int *type, int *free, int *format);
 int McMkDir(int port, int slot, const char *name);
 int McDelete(int port, int slot, const char *name);
 int McFormat(int port, int slot);
 int McUnformat(int port, int slot);
-
 int McPollSema(void);
 __LIBDL_GETTER__ int McGetResult(void);
 
