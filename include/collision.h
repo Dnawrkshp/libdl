dd09ee50ee6b81cad68b68759c96b937bb705ff1
Daniel Gerendasy <danielgerendasy@gmail.com>
2022-01-15 20:42:20 -0800

    Added CollLine_Fix and CollMobysSphere_Fix func decl

--- include/collision.h
+++ include/collision.h
@@ -15,6 +15,23 @@
 
 
 /*
+ * NAME :		CollMobysSphere_Fix
+ * 
+ * DESCRIPTION :
+ * 			
+ * 
+ * NOTES :
+ * 
+ * ARGS : 
+ * 
+ * RETURN :
+ * 
+ * AUTHOR :			Daniel "Dnawrkshp" Gerendasy
+ */
+int CollMobysSphere_Fix(VECTOR position, u64 hitFlag, Moby * moby, u64 a3, float radius);
+
+
+/*
  * NAME :		CollMobysSphere_Fix_GetHitMobies
  * 
  * DESCRIPTION :
@@ -29,6 +46,22 @@
  * AUTHOR :			Daniel "Dnawrkshp" Gerendasy
  */
 __LIBDL_GETTER__ Moby** CollMobysSphere_Fix_GetHitMobies(void);
+
+/*
+ * NAME :		CollLine_Fix
+ * 
+ * DESCRIPTION :
+ * 			
+ * 
+ * NOTES :
+ * 
+ * ARGS : 
+ * 
+ * RETURN :
+ * 
+ * AUTHOR :			Daniel "Dnawrkshp" Gerendasy
+ */
+int CollLine_Fix(VECTOR from, VECTOR to, u64 hitFlag, Moby * moby, u64 t0);
 
 /*
  * NAME :		CollLine_Fix_GetHitMoby
