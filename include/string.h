309ffc7fc3d93ffda3b8c085cd332900eaec3766
Daniel Gerendasy <danielgerendasy@gmail.com>
2022-04-22 11:07:56 -0700

    Added strlen and strcmp to string.h

--- include/string.h
+++ include/string.h
@@ -16,5 +16,7 @@
 __LIBDL_INLINE__ void * memset(void * ptr, int value, int num);
 __LIBDL_INLINE__ void * memcpy(void * destination, const void * source, int num);
 __LIBDL_INLINE__ char * strncpy(char *dest, const char *src, int n);
+__LIBDL_INLINE__ int strlen(char *str);
+__LIBDL_INLINE__ char * strcmp(const char *str1, const char *str2);
 
 #endif // _LIBDL_STRING_H_
