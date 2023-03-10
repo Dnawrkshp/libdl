e3f65601807afd5eb764af1bf5ba302400bca0a6
Daniel Gerendasy <danielgerendasy@gmail.com>
2023-03-10 12:02:46 -0800

    Corrected mobySpawnExplosion

--- include/moby.h
+++ include/moby.h
@@ -847,14 +847,14 @@ void mobyGetJointMatrix(Moby* moby, int jointIdx, MATRIX out);
 /*
  *
  */
-Moby* mobySpawnExplosion(VECTOR pos,float exploScale,int bCameraShake,int numMoonRock,int numSliver,
+Moby* mobySpawnExplosion(u128 pos,float exploScale,int bCameraShake,int numMoonRock,int numSliver,
               int numMoltenRock,int numCloud,u16 numLines,u16 numPlumes,int bDistort,
               int bShell,int bScreenFlash1,int bScreenFlash2,int bGroundRing,int bRandomRing,
               float pointLightRadius,float damageHp,float damageRadius,int damageFlags,
               Moby *pPlatform,int colorCenterCore1,int colorCenterCore2,int colorMoonRock,
               int colorMoonRockGlow,int colorSliver,int colorMoltenRock,int colorCloud,
               int colorShell,int colorRing,int bDirtyRing,Moby *pDamager,
-              Moby *pIgnore,VECTOR externalVel);
+              Moby *pIgnore,u128 externalVel);
 
 int flagIsReturning(Moby* flagMoby);
 int flagIsBeingPickedUp(Moby* flagMoby);
