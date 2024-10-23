9447d499eaaee455160375f2bbda804e0b03151a
Daniel Gerendasy <danielgerendasy@gmail.com>
2024-10-22 23:26:13 -0700

    Add strcpy()

--- include/string.h
+++ include/string.h
@@ -18,6 +18,7 @@ __LIBDL_INLINE__ void * memcpy(void * destination, const void * source, int num)
 __LIBDL_INLINE__ void * memmove(void * destination, const void * source, int num);
 __LIBDL_INLINE__ void * memcmp(void * a, const void * b, int num);
 __LIBDL_INLINE__ char * strncpy(char *dest, const char *src, int n);
+__LIBDL_INLINE__ char * strcpy(char *dest, const char *src);
 __LIBDL_INLINE__ int strlen(char *str);
 __LIBDL_INLINE__ char * strcmp(const char *str1, const char *str2);
 __LIBDL_INLINE__ char * strncmp(const char *str1, const char *str2, int n);
