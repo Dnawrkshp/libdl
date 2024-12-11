2263def7110fbe95bc08ffcee585781a41cc6a76
Daniel Gerendasy <danielgerendasy@gmail.com>
2024-12-10 20:04:39 -0800

    Add decompressWad

---
+++ include/compression.h
@@ -0,0 +1,32 @@
+/***************************************************
+ * FILENAME :    compression.h
+ * 
+ * DESCRIPTION :
+ *     
+ *     
+ * AUTHOR :      Daniel "Dnawrkshp" Gerendasy
+ */
+
+#ifndef _LIBDL_COMPRESSION_H_
+#define _LIBDL_COMPRESSION_H_
+
+#include <tamtypes.h>
+
+/*
+ * NAME :    decompressWad
+ * 
+ * DESCRIPTION :
+ *       
+ * 
+ * NOTES :
+ * 
+ * ARGS : 
+ * 
+ * RETURN :
+ * 
+ * AUTHOR :      Daniel "Dnawrkshp" Gerendasy
+ */
+int decompressWad(void* src, void* dst);
+
+
+#endif // _LIBDL_COMPRESSION_H_
