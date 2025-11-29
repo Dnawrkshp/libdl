dadf58dffbf27889e95a631331c27208ccd63d6c
Daniel Gerendasy <danielgerendasy@gmail.com>
2025-11-28 17:26:32 -0800

    Add collision surface ids and helper functions

--- include/collision.h
+++ include/collision.h
@@ -20,6 +20,26 @@ enum CollisionFlags
   COLLISION_FLAG_IGNORE_DYNAMIC = (1 << 1),
 };
 
+enum CollisionSurfaceIds
+{
+  COLLISION_SURFACE_SWIMMABLE_WATER = 0x00,
+  COLLISION_SURFACE_ACID = 0x01,
+  COLLISION_SURFACE_MAGNET_WALL = 0x02,
+  COLLISION_SURFACE_WATER = 0x03,
+  COLLISION_SURFACE_RING_OF_FIRE = 0x04,
+  COLLISION_SURFACE_ELECTRICITY = 0x05,
+  COLLISION_SURFACE_NOWALK_MAGNET_WALL = 0x06,
+  COLLISION_SURFACE_WALKABLE_1 = 0x07,
+  COLLISION_SURFACE_NONWALKABLE_1 = 0x08,
+  COLLISION_SURFACE_WALKABLE_2 = 0x09,
+  COLLISION_SURFACE_WALKABLE_3 = 0x0A,
+  COLLISION_SURFACE_LETHAL_WATER = 0x0B,
+  COLLISION_SURFACE_NONWALKABLE_2 = 0x0C,
+  COLLISION_SURFACE_LETHAL_WATER_ICE_CUBE = 0x0D,
+  COLLISION_SURFACE_WATER_TRAIL = 0x0E,
+  COLLISION_SURFACE_WALKABLE_4 = 0x0F,
+};
+
 /*
  * NAME :    CollMovingSphere
  * 
@@ -148,6 +168,54 @@ __LIBDL_GETTER__ float* CollLine_Fix_GetHitNormal(void);
  * AUTHOR :      Daniel "Dnawrkshp" Gerendasy
  */
 __LIBDL_GETTER__ u8 CollLine_Fix_GetHitCollisionId(void);
+
+/*
+ * NAME :    CollisionIsWalkable
+ * 
+ * DESCRIPTION :
+ *       
+ * 
+ * NOTES :
+ * 
+ * ARGS : 
+ * 
+ * RETURN :
+ * 
+ * AUTHOR :      Daniel "Dnawrkshp" Gerendasy
+ */
+__LIBDL_GETTER__ int CollisionIsWalkable(int id);
+
+/*
+ * NAME :    CollisionIsNonWalkable
+ * 
+ * DESCRIPTION :
+ *       
+ * 
+ * NOTES :
+ * 
+ * ARGS : 
+ * 
+ * RETURN :
+ * 
+ * AUTHOR :      Daniel "Dnawrkshp" Gerendasy
+ */
+__LIBDL_GETTER__ int CollisionIsNonWalkable(int id);
+
+/*
+ * NAME :    CollisionIsLethal
+ * 
+ * DESCRIPTION :
+ *       
+ * 
+ * NOTES :
+ * 
+ * ARGS : 
+ * 
+ * RETURN :
+ * 
+ * AUTHOR :      Daniel "Dnawrkshp" Gerendasy
+ */
+__LIBDL_GETTER__ int CollisionIsLethal(int id);
 
 
 #endif // _LIBDL_COLLISION_H_
--- src/collision.c
+++ src/collision.c
@@ -38,3 +38,34 @@ u8 CollLine_Fix_GetHitCollisionId(void)
 {
   return COLL_LINE_FIX_HIT_COLLISION_ID;
 }
+
+int CollisionIsWalkable(int id)
+{
+  id &= 0x1f;
+  return id == COLLISION_SURFACE_MAGNET_WALL
+      || id == COLLISION_SURFACE_WALKABLE_1
+      || id == COLLISION_SURFACE_WALKABLE_2
+      || id == COLLISION_SURFACE_WALKABLE_3
+      || id == COLLISION_SURFACE_WALKABLE_4
+      || id == COLLISION_SURFACE_WATER_TRAIL
+      || id == COLLISION_SURFACE_WATER
+      ;
+}
+
+int CollisionIsNonWalkable(int id)
+{
+  id &= 0x1f;
+  return id == COLLISION_SURFACE_NONWALKABLE_1
+      || id == COLLISION_SURFACE_NONWALKABLE_2
+      || id == COLLISION_SURFACE_NOWALK_MAGNET_WALL
+      ;
+}
+
+int CollisionIsLethal(int id)
+{
+  id &= 0x1f;
+  return id == COLLISION_SURFACE_LETHAL_WATER
+      || id == COLLISION_SURFACE_LETHAL_WATER_ICE_CUBE
+      || id == COLLISION_SURFACE_RING_OF_FIRE
+      ;
+}
